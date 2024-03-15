a9e232e3999647cc1503d94c78ca32a984bf7154
Trịnh Quốc Việt <viettq@smartosc.com>
2024-03-15 14:30:25 +0700

    Update route

---
+++ src/app/pages/not-found/not-found.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {NotFoundRoutingModule} from './not-found-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    NotFoundRoutingModule
+  ]
+})
+export class NotFoundModule {
+}
